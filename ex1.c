7f15e2fcccae22227ab8efb9b515ee9161e2e072
liaohanting <2389076695@qq.com>
2019-11-15 03:17:32 -0800

    exam

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    char c[50];
+    int a=0,0=0;
+    printf(c\n);
+    while((c = getchar()) !='\n'
+        if (c=='a'||c='A')
+        a++;
+        else if(c='0')
+        0++;
+    printf("The numer of a is: %d and number of 0 is: %d\n")
+}
---
+++ ex3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+unsigned  long long int a;
+a=1;
+a=~a;
+if (a==0)
+do
+{
+    a<<1
+}while(a!=0)
+printf("%||u\n")
